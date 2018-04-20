a65512af786a785b61cea2700aa7832ab03802de
dhani.057 <dhani.057@gmail.com>
2018-04-20 18:34:16 +0700

    + update Settlement MSTR

--- CONTRAST_WEB/CONTRAST_WEB/Controllers/SettlementListController.cs
+++ CONTRAST_WEB/CONTRAST_WEB/Controllers/SettlementListController.cs
@@ -252,7 +252,7 @@ namespace CONTRAST_WEB.Controllers
                     {
                         ActualCostObject.amount = (int)model.MealSettlement;
                         ActualCostObject.jenis_transaksi = "Meal";
-                        await InsertData.ActualCost(ActualCostObject);
+                        //await InsertData.ActualCost(ActualCostObject);
                         ActualCostObject.start_date_extend = temp_start;
                         ActualCostObject.end_date_extend = temp_end;
                     }
@@ -261,7 +261,7 @@ namespace CONTRAST_WEB.Controllers
                     {
                         ActualCostObject.amount = (int)model.HotelSettlement;
                         ActualCostObject.jenis_transaksi = "Hotel";
-                        await InsertData.ActualCost(ActualCostObject);
+                        //await InsertData.ActualCost(ActualCostObject);
                         ActualCostObject.start_date_extend = temp_start;
                         ActualCostObject.end_date_extend = temp_end;
                     }
@@ -271,7 +271,7 @@ namespace CONTRAST_WEB.Controllers
 
                         ActualCostObject.amount = (int)model.TicketSettlement;
                         ActualCostObject.jenis_transaksi = "Ticket";
-                        await InsertData.ActualCost(ActualCostObject);
+                        //await InsertData.ActualCost(ActualCostObject);
                         ActualCostObject.start_date_extend = temp_start;
                         ActualCostObject.end_date_extend = temp_end;
                     }
@@ -281,7 +281,7 @@ namespace CONTRAST_WEB.Controllers
                         ActualCostObject.amount = (int)model.LaundrySettlement;
                         ActualCostObject.jenis_transaksi = "Laundry";
                         ActualCostObject.path_file = Utility.UploadSettlementReceipt(model.ReceiptFileLaundry, "Laundry_" + ActualCostObject.no_reg + "_" + "_" + DateTime.Now.ToLongDateString() + "_" + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Second);
-                        await InsertData.ActualCost(ActualCostObject);
+                        //await InsertData.ActualCost(ActualCostObject);
                         ActualCostObject.start_date_extend = temp_start;
                         ActualCostObject.end_date_extend = temp_end;
                     }
@@ -291,7 +291,7 @@ namespace CONTRAST_WEB.Controllers
                         ActualCostObject.amount = (int)model.TransportationSettlement;
                         ActualCostObject.jenis_transaksi = "Transportation";
                         ActualCostObject.path_file = Utility.UploadSettlementReceipt(model.ReceiptFileTransportation, "Transport_" + ActualCostObject.no_reg + "_" + "_" + DateTime.Now.ToLongDateString() + "_" + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Second);
-                        await InsertData.ActualCost(ActualCostObject);
+                        //await InsertData.ActualCost(ActualCostObject);
                         ActualCostObject.start_date_extend = temp_start;
                         ActualCostObject.end_date_extend = temp_end;
                     }
@@ -301,7 +301,7 @@ namespace CONTRAST_WEB.Controllers
                         ActualCostObject.amount = (int)model.MiscSettlement;
                         ActualCostObject.jenis_transaksi = "Miscellaneous";
                         ActualCostObject.path_file = Utility.UploadSettlementReceipt(model.ReceiptFileOther, "Other_" + ActualCostObject.no_reg + "_" + "_" + DateTime.Now.ToLongDateString() + "_" + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Second);
-                        await InsertData.ActualCost(ActualCostObject);
+                        //await InsertData.ActualCost(ActualCostObject);
                         ActualCostObject.start_date_extend = temp_start;
                         ActualCostObject.end_date_extend = temp_end;
                     }
@@ -313,10 +313,10 @@ namespace CONTRAST_WEB.Controllers
                     SummarySettlementObject.Summary = await GetData.SummarySettlementInfo(ActualCostObject.group_code);
 
                     //cek update data
-                    if (model.MealSettlement == 0 && model.PreparationSettlement == 0 && model.HotelSettlement == 0 && model.TicketSettlement == 0 && model.LaundrySettlement == 0 && model.TransportationSettlement == 0 && model.MiscSettlement == 0)
-                        await UpdateData.TravelRequest(ActualCostObject.group_code, "1");
-                    else
-                        await UpdateData.TravelRequest(ActualCostObject.group_code, "0");
+                    //if (model.MealSettlement == 0 && model.PreparationSettlement == 0 && model.HotelSettlement == 0 && model.TicketSettlement == 0 && model.LaundrySettlement == 0 && model.TransportationSettlement == 0 && model.MiscSettlement == 0)
+                    //    await UpdateData.TravelRequest(ActualCostObject.group_code, "1");
+                    //else
+                    //    await UpdateData.TravelRequest(ActualCostObject.group_code, "0");
 
                     //migrate ke helper baru
 
@@ -355,13 +355,18 @@ namespace CONTRAST_WEB.Controllers
         }
 
         [HttpPost]
-        [Authorize]
-        [Authorize(Roles = "contrast.user")]
-        [ValidateAntiForgeryToken]
+        //[Authorize]
+        //[Authorize(Roles = "contrast.user")]
+        //[ValidateAntiForgeryToken]
         public ActionResult SummaryPaid(vw_summary_settlement model)
         {
-
             return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult SummaryPaidMSTR(SettlementHelper model)
+        {
+            return RedirectToAction("Index");
         }
 
         // GET: SettlementList
@@ -586,7 +591,7 @@ namespace CONTRAST_WEB.Controllers
                     SummarySettlementObject.Summary.total_ticket = Convert.ToInt32(Convert.ToDouble(SummarySettlementObject.Summary.total_ticket) - SummarySettlementObject.TicketSettlement);
 
                     //if (!model.extend_flag)
-                    return View("SummaryPaid", SummarySettlementObject);
+                    return View("SummaryPaidMSTR", SummarySettlementObject);
                 }
                 else
                 if (sum != null)
@@ -606,8 +611,8 @@ namespace CONTRAST_WEB.Controllers
 
 
         [HttpPost]
-        [Authorize]
-        [Authorize(Roles = "contrast.user")]
+        //[Authorize]
+        //[Authorize(Roles = "contrast.user")]
         //[ValidateAntiForgeryToken]
         public async Task<ActionResult> Print(SettlementPaidHelper model)
         {
