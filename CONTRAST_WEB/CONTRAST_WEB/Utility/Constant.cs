6f1c207d442eb24ecb33936572104f079b160085
npratama <kagenonn2@gmail.com>
2018-04-30 17:46:52 +0700

    - revert to 0ce2b94858a4b6ef6e65ea1ef0da6f93cdebdeb1

--- CONTRAST_WEB/CONTRAST_WEB/Utility/Constant.cs
+++ CONTRAST_WEB/CONTRAST_WEB/Utility/Constant.cs
@@ -9,45 +9,41 @@ namespace CONTRAST_WEB.Models
     public static class Constant
     {
         //dev	       
-        public static string Baseurl = "http://10.85.40.68:90/";
+        //public static string Baseurl = "http://10.85.40.68:90/";
         //local	
         //public static string Baseurl = "http://10.86.110.14:89/";	
         //QA	
         //public static string Baseurl = "http://10.85.26.25/";	
         //Production	
-        //public static string Baseurl = "http://10.185.48.83:444/";
-        //public static string Baseurl = "http://localhost:51687/";
-
+        public static string Baseurl = "http://10.185.48.83:444/";
+        
         //dev
-        public static string PhotoFolder = "http://10.85.40.68:91/PhotoFolder/";
+        //public static string PhotoFolder = "http://10.85.40.68:91/PhotoFolder/";
         //local
         //public static string PhotoFolder = "http://10.86.110.92:56552/PhotoFolder/";
         //QA
         //public static string PhotoFolder = "http://10.85.25.24/PhotoFolder/";
         //Production
-        //public static string PhotoFolder = "https://passport.toyota.astra.co.id:5006/PhotoFolder/";
+        public static string PhotoFolder = "https://passport.toyota.astra.co.id:5006/PhotoFolder/";
 
         //Dev
-        public static string DocumentFolder = "http://10.85.40.68:91/DocumentsFolder/";
+        //public static string DocumentFolder = "http://10.85.40.68:91/TravelDocuments/";
         //local
-        //public static string DocumentFolder = "http://10.86.110.96:88/DocumentsFolder/";
-
+        //public static string DocumentFolder = "http://10.86.110.96:88/TravelDocuments/";
         //Production
-        //public static string DocumentFolder = "https://passport.toyota.astra.co.id:5006/DocumentsFolder/";
+        public static string DocumentFolder = "https://passport.toyota.astra.co.id:5006/TravelDocuments/";
 
         //Dev
-        public static string Attch = "http://10.85.40.68:91/";
+        //public static string Attch = "http://10.85.40.68:91/";
         //Prod
-        //public static string Attch = "http://passport.toyota.astra.co.id:5006/";
-
-
+        public static string Attch = "http://passport.toyota.astra.co.id:5006/";
 
         public static string ImgPath = "~/img";
         public static string TravelExecutionReceiptFolder = "~/ExecutionFolder";
         public static string TravelSettlementReceiptFolder = "~/SettlementFolder";
         public static string TPhotoEmployeeFolder = "~/PhotoFolder";
         public static string LogFolder = "~/Log";
-        public static string TravelDocumentsFolder = "~/DocumentsFolder";
+        public static string TravelDocumentsFolder = "~/TravelDocuments";
 
 
         public static CultureInfo culture = new CultureInfo("id-ID");
